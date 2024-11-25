95ae46949c0857f3492e7b47b7065eb831fd3afd
Billy Yang <me@venti.love>
2024-11-25 21:52:24 +0800

    feat: add new index /tech

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,5 +3,8 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
     "/": [
         "",
+        "/intro",
+        "/posts/",
+        "/tech/",
     ],
 });
---
+++ src/tech/README.md
@@ -0,0 +1,10 @@
+---
+title: 技术
+index: false
+icon: mingcute:pencil-3-fill
+article: false
+---
+
+一般技术相关文章合集。
+
+<Catalog />
